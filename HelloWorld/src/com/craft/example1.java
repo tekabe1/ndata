e89f996d06aca692b7b4fc96c63077b8d6bd27b2
tekabe1 <96458474+tekabe1@users.noreply.github.com>
2021-12-25 19:08:24 -0800

    this is from eclipse

---
+++ HelloWorld/src/com/craft/example1.java
@@ -0,0 +1,10 @@
+package com.craft;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ HelloWorld/src/com/icraft/Example2.java
@@ -0,0 +1,10 @@
+package com.icraft;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello this is a new project");
+	}
+
+}
