911ba8f613676669f26fb979b8969b4f1b93c7d1
tekabe1 <96458474+tekabe1@users.noreply.github.com>
2021-12-25 19:15:41 -0800

    new commit

--- HelloWorld/src/com/icraft/Example2.java
+++ HelloWorld/src/com/icraft/Example2.java
@@ -4,7 +4,7 @@ public class Example2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello this is a new project");
+System.out.println("hello this is a new project now");
 	}
 
 }
